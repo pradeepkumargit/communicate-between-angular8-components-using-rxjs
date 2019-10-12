782c3968fbb6dffb159a2c6ac6a37139e761a03f
pradeepkumargit <pradeep.jobs1926@gmail.com>
2019-10-12 21:09:19 +0530

    updating the mock data for article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Article } from './shared/airticle.interface';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'communicate-between-angular-components-using-rxjs';
+  title = 'communicate-between-angular8-components-using-rxjs';
   articles: Array<Article> = [
     {
       "id": 1,
@@ -23,16 +23,6 @@ export class AppComponent {
       "id": 3,
       "title": "article three ",
       "body": "article three data article three data article three data article three data article three data"
-    },
-    // {
-    //   "id": 4,
-    //   "title": "eum et est occaecati",
-    //   "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
-    // },
-    // {
-    //   "id": 5,
-    //   "title": "nesciunt quas odio",
-    //   "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
-    // }
+    }
   ]
 }
